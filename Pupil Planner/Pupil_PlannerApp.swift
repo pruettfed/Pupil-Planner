f61555eddb84b1fcdca908a64cd1281988200f53
pruettfed <pruettfed@gmail.com>
2023-04-10 23:11:38 -0500

    Initial Commit

---
+++ Pupil Planner/Pupil_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pupil_PlannerApp.swift
+//  Pupil Planner
+//
+//  Created by Pruett Fedorowicz on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Pupil_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
